5afb995de19b3ff1514cee2ac0ee7c929908341a
Yabir Garcia <yabirg@protonmail.com>
2020-12-23 13:26:02 +0100

    Add basic functionalities

--- main.go
+++ main.go
@@ -2,22 +2,120 @@ package main
 import (
 	"fmt"
 	"os"
+	"gopkg.in/yaml.v2"
+	"time"
+	"errors"
+	"io/ioutil"
 )
 
-func readOrCreate(string path){
-	var _, err = os.Stat(path)
+const FILENAME string = ".gqread.yaml"
 
-	if os.IsNotExist(err){
-		var file, err = os.Create(path)
-		if isError(err){
-			fmt.Println("Couldn't create database file at %s", path)
-		}
-		defer file.Close()
+type Document struct{
+	Url string
+	Added int64 // date of the addition
+	Read bool // if the url has been read or not
+	Hn string // link to hn comments if exists
+}
+
+type T struct{
+	Articles []Document
+}
+
+var noDocuments = errors.New("No se ha encontrado ningun archivo")
+var everyThingRead = errors.New("All urls have been read")
+
+func addToDocument(url string, data *T){
+	newArticle := Document {
+		Url: url,
+		Added: time.Now().Unix(),
+		Read: false,
+		Hn: "",
 	}
 
-	fmt.Println("Database file created successfully at %s", path)
+	data.Articles = append([]Document{newArticle},data.Articles...)
+}
+
+func readFromDocument(data *T) (Document, error){
+	
+	if len(data.Articles) == 0 {
+		return Document{}, noDocuments
+	}
+	// find first not read
+	for i, art := range data.Articles{
+		if art.Read == false{
+			return data.Articles[i], nil
+		}
+	}
+
+	return Document{}, everyThingRead
+}
+
+func markLastAsRead(data *T) error{
+	if len(data.Articles) == 0 {
+		return noDocuments
+	}
+	// find first not read
+	for i, art := range data.Articles{
+		if art.Read == false{
+			data.Articles[i].Read = true
+			return nil
+		}
+	}
+
+	return everyThingRead
 }
 
 func main(){
-	data, err :=
+
+	home, homerr := os.UserHomeDir()
+
+	if homerr != nil{
+		fmt.Println("Couldn't find user home directory")
+		return
+	}
+
+	FILEPATH := home + "/" + FILENAME
+
+	file, _ := ioutil.ReadFile(FILEPATH)
+
+	var data T
+	err := yaml.Unmarshal(file, &data)
+
+	if err != nil{
+		fmt.Println("Error reading database file")
+		return 
+	}
+
+	if len(os.Args) == 3 || len(os.Args) == 2{
+		// In this case we want to store an url
+		if os.Args[1] == "add"{
+			addToDocument(os.Args[2], &data)
+		}else if os.Args[1] == "pop"{
+				art, err := readFromDocument(&data)
+ 
+				if err != nil{
+					fmt.Println(err)
+				}
+				article, err := yaml.Marshal(art)
+				fmt.Println(string(article))
+		}else if os.Args[1] == "read"{
+			modifyReadError := markLastAsRead(&data)
+			if modifyReadError != nil{
+				fmt.Println(modifyReadError)
+			}
+		}else{
+			fmt.Println("Unknown argument ", os.Args[1])
+		}
+	} else {
+		fmt.Println("Incorrect number of arguments")
+	}
+
+	marshaled, _ := yaml.Marshal(&data)
+	//fmt.Println(string(marshaled))
+	
+	werror := ioutil.WriteFile(FILEPATH, marshaled, 0644)
+
+	if werror != nil {
+		fmt.Println(werror)
+	}
 }
